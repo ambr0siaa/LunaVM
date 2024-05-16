6d1845797e65bc6519d8f33060348c4a83a54c6a
ambrosia <ambr7156@gmail.com>
2024-05-16 22:30:56 +0500

    Remove useless info about creating directories

--- bilSetup.sh
+++ bilSetup.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-dir=./bin
+dir=bin/
 
 if [ ! -d "$dir" ]; then
     mkdir $dir
-    echo "INFO: $dir has made"
 fi
 
 gcc bil.c -Wall -Wextra -flto -O3 -fno-ident -pipe -fno-ident -z noexecstack -DNDEBUG -o $dir/bil
