399522f25a7c53b94dc6c3b0890123f8e19d99e2
ambrosia <ambr7156@gmail.com>
2024-05-19 18:52:28 +0500

    Add copyright

--- bil.h
+++ bil.h
@@ -1,9 +1,28 @@
 // This custom build system like MakeFile, Cmake and etc
-// But as C single header style library (stb style)
-// bil is crose platform (well, work on windows and linux)
+// But as C single header style library (stb-style)
+// Its crose platform (well, its work on windows and linux)
+
 // This tiny build system has inspired by `https://github.com/tsoding/nobuild`
 
-// Note: probably new fitures not working on windows
+// Copyright (c) 2024 Matthew Sokolkin <sokolkin227@gmail.com>
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #ifndef BIL_H_
 #define BIL_H_
